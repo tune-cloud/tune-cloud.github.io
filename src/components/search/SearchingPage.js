7ac2f41ac6de39b237421583b340671923379c67
matthew-js-porter <matthew.js.porter@jpmchase.com>
2021-06-20 00:32:05 -0400

    fixing issue where on screen keyboard cause search bar to be pushed up.

--- src/components/search/SearchingPage.js
+++ src/components/search/SearchingPage.js
@@ -2,6 +2,8 @@ import SearchBar from "./SearchBar";
 
 export default function SearchingPage(props) {
     return (
-        <SearchBar {...props} searchBarClassName='input-group' searchResultsClassName='list-group search-results'/>
+        <div style={{position: 'fixed', width: '100%'}}>
+            <SearchBar {...props} searchBarClassName='input-group' searchResultsClassName='list-group search-results'/>
+        </div>
     );
 }
