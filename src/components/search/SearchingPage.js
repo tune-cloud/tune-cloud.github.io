21708ab6c8847d81c6fc04ae183908efb3a2ddcb
matthew-js-porter <matthew.js.porter@jpmchase.com>
2021-06-20 00:49:17 -0400

    fixing background color on artist page.

--- src/components/search/SearchingPage.js
+++ src/components/search/SearchingPage.js
@@ -2,7 +2,7 @@ import SearchBar from "./SearchBar";
 
 export default function SearchingPage(props) {
     return (
-        <div style={{position: 'fixed', width: '100%'}}>
+        <div className='search-page'>
             <SearchBar {...props} searchBarClassName='input-group' searchResultsClassName='list-group search-results'/>
         </div>
     );
